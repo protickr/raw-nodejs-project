b9ac3f2a91a09a8c29a0f175c2cd5962201b80fa
Protick Roy <protickrr@gmail.com>
2023-02-27 21:21:35 +0600

    Token service upto extend expiry time

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -1,5 +1,10 @@
 // dependencies
 const data = require("../../lib/data");
+const {
+  hash,
+  parseJSON,
+  createRandomString,
+} = require("../../helpers/utilities");
 
 // module scaffolding
 const handler = {};
@@ -15,14 +20,117 @@ handler.tokenHandler = (requestProperties, callback) => {
 
 handler._token = {};
 
-// create
-handler._token.post = (requestProperties, callback) => {};
+// create token by providing phone number / unique identifier and password
+handler._token.post = (requestProperties, callback) => {
+  const requestBody = requestProperties.body;
+  const phone =
+    typeof requestBody.phone === "string" &&
+    requestBody.phone.trim().length === 11
+      ? requestBody.phone
+      : null;
+
+  const password =
+    typeof requestBody.password === "string" &&
+    requestBody.password.trim().length > 0
+      ? requestBody.password
+      : null;
+
+  if (phone && password) {
+    // if user exists with provided with phone and password provided
+    data.read("users", phone, (err1, userData) => {
+      let hashedPassword = hash(password);
+      let user = { ...parseJSON(userData) }; // only applicable to 1 level of nested objects
+      if (user.password === hashedPassword) {
+        let tokenId = createRandomString(20);
+        let expires = Date.now() + 60 * 60 * 1000;
+        let tokenObj = {
+          phone,
+          id: tokenId,
+          expires,
+        };
+
+        // store the token
+        data.create("tokens", tokenId, tokenObj, (err2) => {
+          if (!err2) {
+            callback(200, tokenObj);
+          } else {
+            callback(500, { error: "there was a problem in the server side!" });
+          }
+        });
+      } else {
+        callback(400, { error: "provided password is not correct!" });
+      }
+    });
+  } else {
+    callback(400, { error: "You have a problem in your request" });
+  }
+};
 
 // read
-handler._token.get = (requestProperties, callback) => {};
+handler._token.get = (requestProperties, callback) => {
+  // check the token number is valid
+  const idReq = requestProperties.queryStrObj.id;
+  const id =
+    typeof idReq === "string" && idReq.trim().length === 20 ? idReq : null;
+  if (id) {
+    data.read("tokens", id, (err1, tokenData) => {
+      if (!err1 && tokenData) {
+        const tokenObj = parseJSON(tokenData);
+        callback(200, tokenObj);
+      } else {
+        callback(404, { error: "token not found !" });
+      }
+    });
+  } else {
+    callback(404, {
+      error: "requested token is not found !",
+    });
+  }
+};
 
 // update
-handler._token.put = (requestProperties, callback) => {};
+// extend token expiry time
+handler._token.put = (requestProperties, callback) => {
+  const idReq = requestProperties.body.id;
+  const id =
+    typeof idReq === "string" && idReq.trim().length === 20 ? idReq : null;
+
+  const extend =
+    typeof requestProperties.body.extend === "boolean" &&
+    requestProperties.body.extend === true
+      ? true
+      : false;
+
+  if (id && extend) {
+    data.read("tokens", id, (err1, tokenData) => {
+      const token = { ...parseJSON(tokenData) };
+      // console.log(token.expires > Date.now());
+
+      if (!err1 && token.expires > Date.now()) {
+        token.expires = Date.now() + 60 * 60 * 1000;
+
+        // store the updated token
+        data.update("tokens", id, token, (err2) => {
+          if (!err2) {
+            callback(200);
+          } else {
+            callback(500, {
+              error: "There was a problem in the server side !",
+            });
+          }
+        });
+      } else {
+        callback(400, {
+          error: "Token already expired !",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "requested token was not found !",
+    });
+  }
+};
 
 // delete
 handler._token.delete = (requestProperties, callback) => {};
--- helpers/utilities.js
+++ helpers/utilities.js
@@ -1,30 +1,53 @@
-// imortant utility function 
+// imortant utility function
 
 // dependencies
-const crypto = require('crypto');
-const environment = require('./environments');
+const crypto = require("crypto");
+const environment = require("./environments");
 
 // module scaffolding
 const utilities = {};
 
 utilities.parseJSON = (jsonStr) => {
-    let output; 
-    try{
-        output = JSON.parse(jsonStr);
-    }catch(err){
-        console.log(err);
-        output = {};
-    }
-    
-    return output;
-}
+  let output;
+  try {
+    output = JSON.parse(jsonStr);
+  } catch (err) {
+    console.log(err);
+    output = {};
+  }
 
+  return output;
+};
+
+// hash string
 utilities.hash = (tobeEnc) => {
-    if(typeof tobeEnc === 'string' && tobeEnc.length > 0){
-        return crypto.createHmac('sha256', environment.secretKey).update(tobeEnc).digest('hex');
-    }else{
-        return false;
+  if (typeof tobeEnc === "string" && tobeEnc.length > 0) {
+    return crypto
+      .createHmac("sha256", environment.secretKey)
+      .update(tobeEnc)
+      .digest("hex");
+  } else {
+    return false;
+  }
+};
+
+// create random string
+utilities.createRandomString = (strLength) => {
+  let length = strLength;
+  length = typeof strLength === "number" && strLength > 0 ? strLength : false;
+  if (length) {
+    let possibleCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let output = "";
+    for (let i = 1; i <= length; i++) {
+      let randomCharacter = possibleCharacters.charAt(
+        Math.floor(Math.random() * possibleCharacters.length)
+      );
+      output += randomCharacter;
     }
+    return output;
+  } else {
+    return false;
+  }
 };
 
 module.exports = utilities;
